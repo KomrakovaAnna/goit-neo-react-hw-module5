5de28b5cdf595a429b1b0730fc6e05233f8fbcd3
Anna Komrakova <komrakova.anna@gmail.com>
2024-10-06 15:13:11 +0100

    fix for 404

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+import style from "./NotFoundPage.module.css";
+
 const NotFoundPage = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className={style.NotFoundPage}>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <Link to="/">Go to home page</Link>
+    </div>
+  );
 };
 export default NotFoundPage;
